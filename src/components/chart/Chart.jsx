4c7bff24e28c8efa21efc1a35f91029c2767594b
Damola Olusakin <olusakinadedamola@gmail.com>
2021-11-11 14:00:37 +0100

    added chart

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,70 @@
+import "./chart.css"
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+export default function Chart() {
+
+  const data = [
+    {
+      name: 'Jan',
+      "Active User": 4000,
+    },
+    {
+      name: 'Feb',
+      "Active User": 3000,
+    },
+    {
+      name: 'Mar',
+      "Active User": 2000,
+    },
+    {
+      name: 'Apr',
+      "Active User": 1000,
+    },
+    {
+      name: 'May',
+      "Active User": 3000,
+    },
+    {
+      name: 'Jun',
+      "Active User": 5000,
+    },
+    {
+      name: 'Jul',
+      "Active User": 2000,
+    },
+    {
+      name: 'Aug',
+      "Active User": 2400,
+    },
+    {
+      name: 'Sept',
+      "Active User": 2300,
+    },
+    {
+      name: 'Oct',
+      "Active User": 3200,
+    },
+    {
+      name: 'Nov',
+      "Active User": 6000,
+    },
+    {
+      name: 'Dec',
+      "Active User": 7000,
+    },
+  ];
+
+  return (
+    <div className="chart">
+      <h3 className="chartTitle">User Analytics</h3>
+       <ResponsiveContainer width="100%" aspect={4 / 1}>
+         <LineChart data={data}>
+           <XAxis dataKey='name' stroke="#5550bd"/>
+           <Line type="monotone" dataKey="Active User" stroke="#5550bd" />
+           <Tooltip />
+           <CartesianGrid stroke="#e0dfdf" strokeDashArray="5 5"/>
+         </LineChart>
+       </ResponsiveContainer>
+    </div>
+  )
+}
