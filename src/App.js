322230e7bc117e59a8f8f91f0a8ce37b58a071c3
Damola Olusakin <olusakinadedamola@gmail.com>
2021-11-14 13:27:04 +0100

    added new user page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
+import NewUser from "./pages/newUSer/NewUser";
 
 function App() {
   return (
@@ -21,6 +22,9 @@ function App() {
           </Route>
           <Route path="/user/:userId">
             <User  />
+          </Route>
+          <Route path="/newUser">
+            <NewUser  />
           </Route>
         </Switch>
       </div>
